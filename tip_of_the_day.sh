8dd4553347711ca43746a227739fd71427efc6df
The-WindowsVista <67670160+The-WindowsVista@users.noreply.github.com>
2022-08-25 01:07:40 +0000

    Add files via upload
    add updated file that allows running from other directories

--- tip_of_the_day.sh
+++ tip_of_the_day.sh
@@ -2,10 +2,10 @@
 # tip_of_the_day.sh
 
 # read the current tip number from .curtip file
-CURTIP=$(cat ~/tipoftheday/.curtip)
+CURTIP=$(cat ./tipoftheday/.curtip)
 
 # get the total number of tip files
-TOTAL_TIPS=$(ls -l ~/tipoftheday/tips/*.txt | wc -l)
+TOTAL_TIPS=$(ls -l ./tipoftheday/tips/*.txt | wc -l)
 
 # flag variable to decide whether to display next tip or not
 next_tip=1
@@ -25,13 +25,13 @@ do
     # Display the tip of the day
     clear
     echo -e  "\n===================================="
-    cat ~/tipoftheday/tips/$TIP_FILE
+    cat $(pwd)/tipoftheday/tips/$TIP_FILE
 
     # Increment the tip number by 1
     CURTIP=$((CURTIP+1))
 
     # write the current tip number to .curtip file
-    echo $CURTIP > ~/tipoftheday/.curtip
+    echo $CURTIP > ./tipoftheday/.curtip
 
     # Prompt user for next tip
     echo "Want to view another tip [yes/no]?"
